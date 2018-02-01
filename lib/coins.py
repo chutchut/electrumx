93e6e9cd34094a105264bf64c4acb1b7c6dcbbb0
Calum Hutton <c.e.hutton@gmx.com>
2018-02-01 22:34:31 +0000

    Remove custom header_hash function for Teslacoin

--- lib/coins.py
+++ lib/coins.py
@@ -1304,16 +1304,3 @@ class Teslacoin(Coin):
     RELAY_FEE = 0.1
     DAEMON = daemon.FakeEstimateFeeDaemon
     HEADER_HASH = None
-
-    @classmethod
-    def header_hash(cls, header):
-        '''Given a header return the hash.'''
-        if cls.HEADER_HASH is None:
-            import scrypt
-            cls.HEADER_HASH = lambda x: scrypt.hash(x, x, 1024, 1, 1, 32)
-
-        version, = struct.unpack('<I', header[:4])
-        if version > 6:
-            return super().header_hash(header)
-        else:
-            return cls.HEADER_HASH(header)
